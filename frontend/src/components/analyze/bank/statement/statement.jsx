db44065f5c72c21ecea0ed9cb28cbfe5774d9291
Abhishek Verma <110097168+AV07011997@users.noreply.github.com>
2023-05-20 10:55:53 +0530

    filter_element_html_css

--- frontend/src/components/analyze/bank/statement/statement.jsx
+++ frontend/src/components/analyze/bank/statement/statement.jsx
@@ -1,27 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../../../../utilities/navbar/navbar";
 import { postApi } from "../../../../callapi";
+import { Loader } from "rsuite";
+import "./statement.css";
 
 const AnalyzeStatement = (leadID) => {
-  useEffect(() => {
-    var statementsArray = [];
-    postApi("analyze/" + APIADDRESS.ANALYZEBANKSUMMARY, {
-      leadID: localStorage.getItem("leadID"),
-    }).then((res) => {
-      const temp = JSON.parse(res)[0].data;
-      temp.forEach((element) => {
-        if (element[0] != null) {
-          statementsArray.push(element[0]);
-        }
-      });
-      setTable(statementsArray);
-    });
-  }, []);
+  const [table, setTable] = useState();
+  // useEffect(() => {
+  //   var statementsArray = [];
+  //   postApi("analyze/" + APIADDRESS.ANALYZEBANKSUMMARY, {
+  //     leadID: localStorage.getItem("leadID"),
+  //   }).then((res) => {
+  //     const temp = JSON.parse(res)[0].data;
+  //     temp.forEach((element) => {
+  //       if (element[0] != null) {
+  //         statementsArray.push(element[0]);
+  //       }
+  //     });
+  //     setTable(statementsArray);
+  //   });
+  // }, []);
+
+  const fetchvariables = () => {
+    console.log("called");
+    const txn_from = document?.getElementById("txn_date_from").value;
+    const txn_to = document?.getElementById("txn_date_to").value;
+    const debit_from = document?.getElementById("debit_amount_from").value;
+    const debit_to = document?.getElementById("debit_amount_to").value;
+    const credit_from = document?.getElementById("credit_amount_from").value;
+    const credit_to = document?.getElementById("credit_amount_to").value;
+    const closing_bal_from = document?.getElementById(
+      "closing_balance_amount_from"
+    ).value;
+    const closing_bal_to = document?.getElementById(
+      "closing_balance_amount_to"
+    ).value;
+    console.log(debit_from);
+    console.log(txn_from);
+  };
+
   return (
     <div>
       <NavBar></NavBar>
 
-      <div>
+      {/* <div>
         <div className="div_table1_monthwise">
           {table ? (
             <table className="table1_monthwise">
@@ -79,6 +101,49 @@ const AnalyzeStatement = (leadID) => {
               content="loading..."
             ></Loader>
           )}
+        </div>
+      </div> */}
+      <div className="filter_element_div_block">
+        <div className="filter_element">
+          <h6>Transaction Date</h6>
+          <span>From</span>
+          <input type="date" id="txn_date_from"></input>
+          <span>To</span>
+          <input type="date" id="txn_date_to"></input>
+        </div>
+        <div className="filter_element">
+          <h6>Debited Amount</h6>
+          <span>From</span>
+          <input type="text" id="debit_amount_from"></input>
+          <span>To</span>
+          <input type="text" id="debit_amount_to"></input>
+        </div>
+
+        <div className="filter_element">
+          <h6>Credited Amount</h6>
+          <span>From</span>
+          <input type="text" id="credit_amount_from"></input>
+          <span>To</span>
+          <input type="text" id="credit_amount_to"></input>
+        </div>
+
+        <div className="filter_element">
+          <h6>Closing Balance Amount</h6>
+          <span>From</span>
+          <input type="text" id="closing_balance_amount_from"></input>
+          <span>To</span>
+          <input type="text" id="closing_balance_amount_to"></input>
+        </div>
+        <br></br>
+        <div className="submit_statement">
+          <button
+            className="button_statement"
+            onClick={() => {
+              fetchvariables();
+            }}
+          >
+            Submit
+          </button>
         </div>
       </div>
     </div>
